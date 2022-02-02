5bf057930449b0e6405ae667fff7eb6d9d532b26
Mahid Ahmad <31464238+mahid797@users.noreply.github.com>
2022-02-02 07:20:42 -0500

    Update DictionaryConnection.java

--- src/ca/ubc/cs317/dict/net/DictionaryConnection.java
+++ src/ca/ubc/cs317/dict/net/DictionaryConnection.java
@@ -73,12 +73,15 @@ public class DictionaryConnection {
      */
     public synchronized void close() {
 
-        // TODO Add your code here
+        socketOutput.println("quit");
+
         try {
-            this.socket.close();
-            this.socketInput.close();
-            this.socketOutput.close();
+            socket.close();
+            socketInput.close();
+            socketOutput.close();
+
         } catch (Exception e) {
+
             e.printStackTrace();
             System.exit(1);
         }
